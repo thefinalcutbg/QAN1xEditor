e0efceddf8280f84608cc0a7d9e016da0708250f
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-03-15 00:51:05 +0200

    added Apple signing and notarization

--- installer/macos/InstallScript.sh
+++ installer/macos/InstallScript.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 APP="$PWD/../files/QAN1xEditor.app" #the compiled bundle
-
+INSTALLER="$PWD/../compiled/qan1xeditor-macos.dmg"
 QDEPLOY="$PWD/../../../Qt/6.5.3/macos/bin/macdeployqt" #macdeployqt location
 
-$QDEPLOY $APP #deploying the app
+$QDEPLOY $APP -sign-for-notarization="Developer ID Application: Hristo Konstantinov" #deploying the app
+
+#deleting the old dmg installer
+Rm -R $INSTALLER
 
 #creating dmg installer using create-dmg
 create-dmg \
@@ -14,9 +17,18 @@ create-dmg \
   --window-pos 200 110 \
   --window-size 605 360 \
   --icon-size 72 \
-  --icon "QAN1xEditor.app" 250 170 \
-  --app-drop-link 460 170 \
-  --add-file "Instructions.txt" "Instructions.txt" 460 30 \
+  --icon "QAN1xEditor.app" 250 190 \
+  --app-drop-link 460 190 \
   --hide-extension "QAN1xEditor.app" \
-  "$PWD/../compiled/qan1xeditor-1.2-macos.dmg" \
+  $INSTALLER \
   "$PWD/../files/"
+
+codesign --options runtime --timestamp -s "Developer ID Application: Hristo Konstantinov" $INSTALLER
+
+xcrun notarytool submit $INSTALLER --keychain-profile "DeployProfile" --wait
+
+xcrun stapler staple $INSTALLER
+
+spctl --assess -vv --type install $INSTALLER
+
+rm -R $APP
