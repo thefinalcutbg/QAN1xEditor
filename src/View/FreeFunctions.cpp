24225518ac8a0c8241e2e60e853cf0f0c67a977c
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-04-23 02:24:15 +0300

    version update

--- src/View/FreeFunctions.cpp
+++ src/View/FreeFunctions.cpp
@@ -54,7 +54,7 @@ bool FreeFn::getUpdate()
     reply->deleteLater();
 
     //Current version
-    constexpr int current_ver = 3;
+    constexpr int current_ver = 4;
 
     if (!ok || current_ver >= latest_ver) return false;
 
